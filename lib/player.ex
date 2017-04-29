3ba9cb91bdcf4f64a8c722b71a532ea1e6418d9e
Eric Tobin <eatobin@gmail.com>
2017-04-29 17:21:26 -0500

    add player and obtain symbol

---
+++ lib/player.ex
@@ -0,0 +1,19 @@
+defmodule Player do
+  defstruct [:symbol, :goes_first, :my_turn]
+
+  @bad_symbols ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+
+  def obtain_symbol() do
+    p_symbol = IO.gets("Please choose your symbol (anything except the numbers 0 - 8): ") |> String.first
+    validate_symbol(p_symbol)
+  end
+
+  def validate_symbol(p_symbol) do
+    if (Enum.member?(@bad_symbols, p_symbol)) do
+      IO.write("\nSorry, but that symbol is not allowed. Try again...\n\n")
+      obtain_symbol()
+    else
+      p_symbol
+    end
+  end
+end
