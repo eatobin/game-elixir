3b8145ced6fa0a8ae9916585720ff0579d95329e
Eric Tobin <eatobin@gmail.com>
2017-04-30 08:49:14 -0500

    player testing progress

--- test/player_test.exs
+++ test/player_test.exs
@@ -2,4 +2,8 @@ ExUnit.start
 
 defmodule PlayerTest do
   use ExUnit.Case, async: true
+
+  test "make_player" do
+    assert Player.make_player("?", true, true) == %Player{goes_first: true, my_turn: true, p_symbol: "?"}
+  end
 end
