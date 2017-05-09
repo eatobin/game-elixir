bdac5f16d7db6cc184ebec61ceada1da98f6a0bf
Eric Tobin <eatobin@gmail.com>
2017-05-09 09:26:18 -0500

    repaired tests

--- test/rules_test.exs
+++ test/rules_test.exs
@@ -3,10 +3,10 @@ ExUnit.start
 defmodule RulesTest do
   use ExUnit.Case, async: true
 
-  @winning_row_hal ["H", 1, "H"]
-  @winning_row_human ["*", 1, "*"]
-  @not_winning_row ["H", 1, "*"]
-  @tie_game ["*", "*", "H"]
+  @winning_row_hal ["H", 1, 2, 3, "H", 5, 6, 7, "H"]
+  @winning_row_human [0, 1, "E", 3, "E", 5, "E", 7, 8]
+  @not_winning_row [0, 1, "E", 3, "H", 5, "E", 7, 8]
+  @tie_game ["T", "T", "T", "T", "T", "T", "T", "T", "T"]
 
   test "check for game won (winning row)" do
     assert Rules.game_won?(@winning_row_hal) == true
@@ -16,6 +16,7 @@ defmodule RulesTest do
 
   test "check for tie board" do
     assert Rules.tie?(@tie_game) == true
+    assert Rules.tie?(@winning_row_human) == false
     assert Rules.tie?(@not_winning_row) == false
   end
 end
