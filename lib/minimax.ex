9a4eff22d01f85c2a99531402a80511745b19f3c
Eric Tobin <eatobin@gmail.com>
2017-06-21 17:02:01 -0500

    progress...

--- lib/minimax.ex
+++ lib/minimax.ex
@@ -1,37 +1,4 @@
 defmodule Minimax do
-  @moduledoc """
-  lets visualize the above game state
-
-                                     O |   | X
-                                     ---------
-                                     X |   | X
-                                     ---------
-                                       | O | O
-
-                               //       ||        \\
-
-                  O | X | X          O |   | X        O |   | X
-                  ---------          ---------        ---------
-                  X |   | X          X | X | X        X |   | X
-                  ---------          ---------        ---------
-                    | O | O            | O | O        X | O | O
-
-                //          \\                     //          \\
-
-          O | X | X          O | X | X        O | O | X       O |   | X
-          ---------          ---------        ---------       ---------
-          X | O | X          X |   | X        X |   | X       X | O | X
-          ---------          ---------        ---------       ---------
-            | O | O          O | O | O        X | O | O       X | O | O
-
-                                          //
-
-                                     O | O | X
-                                     ---------
-                                     X | X | X
-                                     ---------
-                                     O | O | O
-  """
 
   # @orig_board ["X","O","X","O","O","X",6,7,8]
 
@@ -41,49 +8,46 @@ defmodule Minimax do
   # ai
   @opponent "O"
 
-def is_moves_left?(board) do
-  Enum.any?(board, fn(x) -> is_integer(x) end)
-end
-
-  @doc """
-  the main minimax function
-  """
-  def minimax(new_board, player) do
-    # available spots
-    avail_spots = empty_indexies(new_board)
-
-    score =
-      cond do
-        Minimax.winning(new_board, @hu_player) -> -10
-        Minimax.winning(new_board, @ai_player) -> 10
-        true -> 0
-      end
-    moves = []
+  def is_moves_left?(board) do
+    Enum.any?(board, fn(x) -> is_integer(x) end)
   end
 
-  @doc """
-  returns list of the indexes of empty spots on the board
-  """
-  def empty_indexies(board) do
+  def won?([x0,x1,x2,x3,x4,x5,x6,x7,x8]) do
+    (x0 == x1) and (x1 == x2) or
+    (x3 == x4) and (x4 == x5) or
+    (x6 == x7) and (x7 == x8) or
+    (x0 == x3) and (x3 == x6) or
+    (x1 == x4) and (x4 == x7) or
+    (x2 == x5) and (x5 == x8) or
+    (x0 == x4) and (x4 == x8) or
+    (x2 == x4) and (x4 == x6)
+  end
+
+  def evaluate(board, is_max?) do
+    cond do
+      won?(board) and is_max? -> 10
+      won?(board) and !is_max? -> -10
+      true -> 0
+    end
+  end
+
+  def toggle_turn(is_max?) do
+    !is_max?
+  end
+
+  def possible_moves(board) do
     Enum.filter(board, fn(x) -> is_integer(x) end)
   end
 
-  @doc """
-  winning combinations using the board indexies
-  """
-  def winning([x0,x1,x2,x3,x4,x5,x6,x7,x8]) do
+  def minimax(board, depth, is_max?) do
+    score = evaluate(board, is_max?)
+
     cond do
-      (x0 == x1) and (x1 == x2) or
-      (x3 == x4) and (x4 == x5) or
-      (x6 == x7) and (x7 == x8) or
-      (x0 == x3) and (x3 == x6) or
-      (x1 == x4) and (x4 == x7) or
-      (x2 == x5) and (x5 == x8) or
-      (x0 == x4) and (x4 == x8) or
-      (x2 == x4) and (x4 == x6) and
-      # x0 == @player -> 10
-      # player == @opponent -> -10
-      true -> 0
+      score == 10 -> score
+      score == -10 -> score
+      !is_moves_left?(board) -> 0
+      is_max? -> pm = possible_moves(board)
+                 pm
     end
   end
 end
