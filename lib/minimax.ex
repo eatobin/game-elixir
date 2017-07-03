a82adfdbe76067e9c685709cee769c8d401ac684
Eric Tobin <eatobin@gmail.com>
2017-07-03 11:26:33 -0500

    progress

--- lib/minimax.ex
+++ lib/minimax.ex
@@ -66,6 +66,18 @@ defmodule Minimax do
     List.replace_at(game_state.board, move, game_state.player)
   end
 
+  def is_gameover(game_state) do
+    [x0,x1,x2,x3,x4,x5,x6,x7,x8] = game_state.board
+    (x0 == x1) and (x1 == x2) or
+    (x3 == x4) and (x4 == x5) or
+    (x6 == x7) and (x7 == x8) or
+    (x0 == x3) and (x3 == x6) or
+    (x1 == x4) and (x4 == x7) or
+    (x2 == x5) and (x5 == x8) or
+    (x0 == x4) and (x4 == x8) or
+    (x2 == x4) and (x4 == x6)
+  end
+
   def ident(x) do
     if is_bitstring(x) do
       x
