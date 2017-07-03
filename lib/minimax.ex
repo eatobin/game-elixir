47a99b00561dabb53823ba77a4c00072b45a2629
Eric Tobin <eatobin@gmail.com>
2017-07-03 13:43:25 -0500

    fixed next_state!

--- lib/minimax.ex
+++ lib/minimax.ex
@@ -30,29 +30,29 @@ defmodule Minimax do
   # ai
   # @opponent "O"
 
-  def is_moves_left?(board) do
-    Enum.any?(board, fn(x) -> is_integer(x) end)
-  end
-
-  def won?([x0,x1,x2,x3,x4,x5,x6,x7,x8]) do
-    (x0 == x1) and (x1 == x2) or
-    (x3 == x4) and (x4 == x5) or
-    (x6 == x7) and (x7 == x8) or
-    (x0 == x3) and (x3 == x6) or
-    (x1 == x4) and (x4 == x7) or
-    (x2 == x5) and (x5 == x8) or
-    (x0 == x4) and (x4 == x8) or
-    (x2 == x4) and (x4 == x6)
-  end
-
-  def evaluate(board, is_max?) do
-    cond do
-      won?(board) and is_max? -> 10
-      won?(board) and !is_max? -> -10
-      !is_moves_left?(board) -> 0
-      true -> nil
-    end
-  end
+  # def is_moves_left?(board) do
+  #   Enum.any?(board, fn(x) -> is_integer(x) end)
+  # end
+  #
+  # def won?([x0,x1,x2,x3,x4,x5,x6,x7,x8]) do
+  #   (x0 == x1) and (x1 == x2) or
+  #   (x3 == x4) and (x4 == x5) or
+  #   (x6 == x7) and (x7 == x8) or
+  #   (x0 == x3) and (x3 == x6) or
+  #   (x1 == x4) and (x4 == x7) or
+  #   (x2 == x5) and (x5 == x8) or
+  #   (x0 == x4) and (x4 == x8) or
+  #   (x2 == x4) and (x4 == x6)
+  # end
+  #
+  # def evaluate(board, is_max?) do
+  #   cond do
+  #     won?(board) and is_max? -> 10
+  #     won?(board) and !is_max? -> -10
+  #     !is_moves_left?(board) -> 0
+  #     true -> nil
+  #   end
+  # end
 
   # def toggle_turn(is_max?) do
   #   !is_max?
@@ -63,7 +63,7 @@ defmodule Minimax do
   end
 
   def next_state(game_state, move) do
-    List.replace_at(game_state.board, move, game_state.player)
+    %GameState{board: List.replace_at(game_state.board, move, game_state.player), player: "X"}
   end
 
   def is_gameover(game_state) do
@@ -78,28 +78,58 @@ defmodule Minimax do
     (x2 == x4) and (x4 == x6)
   end
 
-  def ident(x) do
-    if is_bitstring(x) do
-      x
+  # def min_play(game_state) do
+  #   if is_gameover(game_state) do
+  #     -10
+  #   else
+  #     Enum.min(Enum.map(get_available_moves(game_state), fn(x) -> max_play(next_state(game_state, x)) end))
+  #   end
+  # end
+
+
+  def min_play(game_state) do
+    if is_gameover(game_state) do
+      "Done"
     else
-      x + 1
+      Enum.map(get_available_moves(game_state), fn(x) -> min_play(next_state(game_state, x)) end)
     end
   end
 
-  def ident2(x) do
-    cond do
-      is_bitstring(x) ->
-        x
-      x == 0 ->
-        0
-      true ->
-        ident2(x - 1)
-    end
-  end
 
-  def map_it() do
-    Enum.map(["X","O","X","O","O","X",6,"X",12], &ident2/1)
-  end
+  # def max_play(game_state) do
+  #   if is_gameover(game_state) do
+  #     10
+  #   else
+  #     Enum.max(Enum.map(get_available_moves(game_state), fn(x) -> min_play(next_state(game_state, x)) end))
+  #   end
+  # end
+  #
+  # def minimax(game_state) do
+  #   Enum.max(Enum.map(get_available_moves(game_state), fn(x) -> min_play(next_state(game_state, x)) end))
+  # end
+
+  # def ident(x) do
+  #   if is_bitstring(x) do
+  #     x
+  #   else
+  #     x + 1
+  #   end
+  # end
+  #
+  # def ident2(x) do
+  #   cond do
+  #     is_bitstring(x) ->
+  #       x
+  #     x == 0 ->
+  #       0
+  #     true ->
+  #       ident2(x - 1)
+  #   end
+  # end
+  #
+  # def map_it() do
+  #   Enum.map(["X","O","X","O","O","X",6,"X",12], &ident2/1)
+  # end
 
   # def minimax(board, is_max?) do
   #   score = evaluate(board, is_max?)
