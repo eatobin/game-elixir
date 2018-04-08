5dae6020367b0745ebde39851d7eca32a6a2c921
Eric Tobin <eatobin@gmail.com>
2018-04-08 14:54:30 -0500

    new gitignore and elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Game.Mixfile do
   def project do
     [app: :game,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.6",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
