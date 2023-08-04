d879766ade5546e701b278ab53013dc4075b2f30
Eric Tobin <eatobin@gmail.com>
2023-08-03 22:29:38 -0500

    bump Elixir to 15

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Game.Mixfile do
     [
       app: :game,
       version: "0.0.1",
-      elixir: "~> 1.12",
+      elixir: "~> 1.15",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
